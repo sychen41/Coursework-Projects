0ed5b210e7fd593549948436e1c756d4323104fb
DarkJediXP <seanmcdaniel@s-dyn-191-133.dtcc.edu>
2014-04-03 15:39:39 -0400

    Added TONS of images, added event listeners to Player
    I added a lot of images along with two sprite sheets. I also created a
    player class with event listeners. I player will be created on the
    beginning screen he is movable with the arrow keys.

--- game/CaptainMath.py
+++ game/CaptainMath.py
@@ -17,13 +17,70 @@ class font(spyral.Sprite):
         self.y = 0
         self.moving = False
 
+class Player(spyral.Sprite):
+    def __init__(self, scene):
+        spyral.Sprite.__init__(self, scene)
+        global playerColor
+        playerColor = "red"
+        if(playerColor == "red"):
+            self.image = spyral.image.Image(filename = "images/mainPlayerRedImages/RedPlayerShootingLaserRight.png", size = None)
+        elif(playerColor == "blue"):
+            self.image = spyral.image.Image(filename = "images/entireScenes/hand_blue.png", size = None)
+        self.x = WIDTH/2
+        self.y = HEIGHT - 200
+        self.moving = False
+
+        left = "left"
+        right="right"
+        up = "up"
+        down = "down"
+        enter = "]"
+        spyral.event.register("input.keyboard.down."+left, self.move_left)
+        spyral.event.register("input.keyboard.down."+right, self.move_right)
+        spyral.event.register("input.keyboard.up."+left, self.stop_move)
+        spyral.event.register("input.keyboard.up."+right, self.stop_move)
+        spyral.event.register("input.keyboard.down."+up, self.move_up)
+        spyral.event.register("input.keyboard.down."+down, self.move_down)
+        spyral.event.register("input.keyboard.up."+up, self.stop_move)
+        spyral.event.register("input.keyboard.up."+down, self.stop_move)
+        spyral.event.register("input.keyboard.up."+enter, self.stop_move)
+        spyral.event.register("input.mouse.left.click", self.askquest)
+        spyral.event.register("director.update", self.update)
+        
+    def move_left(self):
+        self.moving = 'left'
+    def move_right(self):
+        self.moving = 'right'
+    def move_up(self):
+        self.moving = 'up'
+    def move_down(self):
+        self.moving = 'down'
+    def place_piece(self):
+        self.moving = 'place_piece'
+    def stop_move(self):
+        self.moving = False
+    def _reset(self):
+        self.y = HEIGHT/2
+    def askquest(self):
+        print "askquest"
+    def update(self, delta):
+        paddle_velocity = 250
+        
+        if self.moving == 'left':
+            self.x -= paddle_velocity * delta
+        elif self.moving == 'right':
+            self.x += paddle_velocity * delta
+        elif self.moving == 'up':
+            self.y -= paddle_velocity * delta
+        elif self.moving == 'down':
+            self.y += paddle_velocity * delta
+
 class CaptainMath(spyral.Scene):
     def __init__(self, *args, **kwargs):
         global manager
         spyral.Scene.__init__(self, SIZE)
-        #self.background = spyral.Image(size=SIZE).fill(BG_COLOR)
-        self.background = spyral.Image("images/Begin.png")
-        
+        self.background = spyral.Image("images/entireScenes/Begin.png")
+        self.player = Player(self)
         left = "left"
         right="right"
         up = "up"
@@ -33,11 +90,7 @@ class CaptainMath(spyral.Scene):
         spyral.event.register("system.quit", spyral.director.pop)
         spyral.event.register("director.update", self.update)
         spyral.event.register("input.keyboard.down.q", spyral.director.pop)
-        spyral.event.register("input.keyboard.down."+up, self.askquest)
-    def askquest(self):
-        self.background = spyral.Image("images/MathmuncherboardwithsomestuffONed.png");
+
     def update(self, delta):
         print "hello world"
-
-        
-
+        print "hey"
