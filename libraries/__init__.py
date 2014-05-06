34f05be033bc2c5240126bcb39b29bb6aef29dad
acbart <acbart@vt.edu>
2014-05-06 11:23:39 -0400

    Made sure that spyral could be found

--- libraries/__init__.py
+++ libraries/__init__.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 def setup_path():
+    sys.path.insert(0, 'libraries')
     modules = ['requests', 'envoy', 'sugargame', 'spyral', 'parsley']
     for module in modules:
         sys.path.insert(0, os.path.abspath('libraries/%s/' % module))
