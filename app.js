bf3da3445d87bdc997075cb8334180d95557b05d
Shiyi Chen <sychen@udel.edu>
2016-10-24 00:58:33 -0400

    success changed database
    from mongodb to mysql(local)

--- app.js
+++ app.js
@@ -5,23 +5,21 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var mongoose = require('mongoose');
 var database = require('./config/database');
 
 var app = express();
 
-// view engine setup. we will make this a
-// single page application, thus no view engine needed
-// only a single page 'index.html' needed.
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
+// view engine setup. Doesn't matter, since we
+// are not using this view engine. Any visit to
+// home page will directly call app.get('/api/todos',......)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// we can NOT use this, maybe because of res.sendfile method
+// wondering if we can use res.send method, maybe can't either
 //app.get('*', function(req, res){
 //    res.sendfile('./public/index.html');
 //} );
-mongoose.connect(database.remoteUrl);
-var Todo = mongoose.model('Todo', {
-    text: String
-});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -32,7 +30,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride());
 
-//routes
+//routes with mongodb
+/*
+var mongoose = require('mongoose');
+mongoose.connect(database.remoteUrl);
+var Todo = mongoose.model('Todo', {
+text: String
+});
 app.get('/api/todos', function(req, res) {
     Todo.find(function(err, todos) {
         if (err)
@@ -65,6 +69,67 @@ app.delete('/api/todos/:todo_id', function(req, res) {
                 res.send(err);
             res.json(todos);
         });
+    });
+});
+*/
+var connectionProvider = require('./mysqlServer/connectionProvider');
+app.get('/api/todos', function(req, res) {
+    var connection = connectionProvider.connectionProdiver.startConnection();
+    if (connection) {
+        connection.query("select * from todo", function(err, todos){
+            if (err)
+                res.send(err);
+            res.json(todos);
+        });
+    }
+    connection.end(function(err) {
+        if (!err)
+            console.log("connection closed after get all todos");
+        else
+            console.log("connection close error: " + err);
+    });
+});
+app.post('/api/todos', function(req, res) {
+    var connection = connectionProvider.connectionProdiver.startConnection();
+    if (connection) {
+        connection.query("insert into todo set?", req.body, function (err) {
+            if (err)
+                res.send(err);
+        });
+        // refresh the todos after insert
+        connection.query("select text from todo", function(err, todos){
+            if (err)
+                res.send(err);
+            res.json(todos);
+        });
+    }
+    connection.end(function(err) {
+        if (!err)
+            console.log("connection closed after insert a todo");
+        else
+            console.log("connection close error: " + err);
+    });
+});
+app.delete('/api/todos/:todo_id', function(req, res) {
+    var connection = connectionProvider.connectionProdiver.startConnection();
+    if (connection) {
+        connection.query("delete from todo where id=?", req.params.todo_id, function(err) {
+            if (err)
+                res.send(err);
+        });
+        // refresh the todos after delete
+        connection.query("select text from todo", function(err, todos){
+            if (err)
+                res.send(err);
+            res.json(todos);
+        });
+
+    }
+    connection.end(function(err) {
+        if (!err)
+            console.log("connection closed after delete a todo");
+        else
+            console.log("connection close error: " + err);
     });
 });
 // catch 404 and forward to error handler
